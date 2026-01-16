efa8150dffbb833cf70d7eea44bb44142a6ea49c
Ohad Schneider <ohad188@gmail.com>
2026-01-16 18:26:31 +0200

    normalize package folder structure

--- package.ps1
+++ package.ps1
@@ -1,26 +1,30 @@
 param(
-    $BuildFolder = $PSScriptRoot,
+    $ServerBuild = "$PSScriptRoot/server",
+    $ClientBuild = "$PSScriptRoot/client/dist",
     [switch]$SkipPackageCleanupConfirmation
 )
 
 $ErrorActionPreference = 'Stop'
 
-Write-Host "Packaging build files from '$BuildFolder'..."
-$buildFiles = Get-ChildItem -LiteralPath $BuildFolder -Recurse -Force
-Write-Host "Build files: $($buildFiles.FullName -join ', ')"
+$buildFilesServer = Get-ChildItem -LiteralPath $ServerBuild -Recurse -Force
+Write-Host "Server build files: $($buildFilesServer.FullName -join ', ')"
+
+$buildFilesClient = Get-ChildItem -LiteralPath $ClientBuild -Recurse -Force
+Write-Host "Client build files: $($buildFilesClient.FullName -join ', ')"
 
 $pkgDir = "$PSScriptRoot/package"
+$publicDir = "$pkgDir/public"
 Write-Host "Preparing package folder at '$pkgDir'..."
 
 if (Test-Path -LiteralPath $pkgDir) {
     Write-Host "Cleaning up existing package folder..."
-    Remove-Item -LiteralPath $pkgDir -Recurse -Force -Confirm:(!$SkipPackageCleanupConfirmation)
+    Remove-Item -LiteralPath $pkgDir -Recurse -Force -Confirm:(!$SkipPackageCleanupConfirmation) -Verbose
 }
 
-New-Item -ItemType Directory -Path $pkgDir -Verbose
+New-Item -ItemType Directory -Path $publicDir -Force -Verbose
 
 Write-Host "Copying built files to package folder..."
-Copy-Item -Recurse -Force "$BuildFolder/client" $pkgDir
-Copy-Item -Recurse -Force "$BuildFolder/server" $pkgDir
+Copy-Item -Path "$ServerBuild/*" -Destination $pkgDir -Recurse -Force -Verbose 
+Copy-Item -Path "$ClientBuild/*" -Destination $publicDir -Recurse -Force -Verbose
 
 Write-Host "Production bundle ready at '$pkgDir'"
